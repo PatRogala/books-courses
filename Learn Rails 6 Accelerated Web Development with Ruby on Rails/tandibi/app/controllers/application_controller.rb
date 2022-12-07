f1f1a39f373705056d5f17c3761ebc0dc177399f
Patryk Rogala <rogala.patryk@pm.me>
2022-12-07 20:26:39 +0100

    Setting Up the Layout

--- Learn Rails 6 Accelerated Web Development with Ruby on Rails/tandibi/app/controllers/application_controller.rb
+++ Learn Rails 6 Accelerated Web Development with Ruby on Rails/tandibi/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    devise_controller? ? 'session' : 'application'
+  end
 end
---
+++ Learn Rails 6 Accelerated Web Development with Ruby on Rails/tandibi/app/javascript/css/tailwind.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
