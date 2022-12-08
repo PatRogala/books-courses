bf05de961b0aa20b516e1e8a22887334e0fcdaa0
Patryk Rogala <rogala.patryk@pm.me>
2022-12-08 23:46:39 +0100

    Customizing the Sign-Up Page

--- Learn Rails 6 Accelerated Web Development with Ruby on Rails/tandibi/app/controllers/application_controller.rb
+++ Learn Rails 6 Accelerated Web Development with Ruby on Rails/tandibi/app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :config_devise_params, if: :devise_controller?
+
   layout :layout_by_resource
 
   private
 
   def layout_by_resource
     devise_controller? ? 'session' : 'application'
+  end
+
+  protected
+
+  def config_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[
+                                        first_name
+                                        last_name
+                                        username
+                                        email
+                                        password
+                                        password_confirmation
+                                      ])
   end
 end
