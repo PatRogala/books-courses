265f490c279c6248ff25692c14ef4db3d7b3fd80
Patryk Rogala <rogala.patryk@pm.me>
2023-05-09 10:34:57 +0200

    Forwarding user + authorization

--- Ruby on Rails Tutorial 7/sample_app/app/controllers/users_controller.rb
+++ Ruby on Rails Tutorial 7/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -19,10 +22,37 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email,
                                  :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url, status: :see_other
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
--- Ruby on Rails Tutorial 7/sample_app/app/helpers/sessions_helper.rb
+++ Ruby on Rails Tutorial 7/sample_app/app/helpers/sessions_helper.rb
@@ -26,6 +26,10 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user && user == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
@@ -40,5 +44,9 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
