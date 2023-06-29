1ee8473350e5afce9de98333bb523369a35138e3
Patryk Rogala <rogala.patryk@pm.me>
2023-06-29 22:46:50 +0200

    Always Use Canonical Routes that Conform to Rails' Defaults

--- Sustainable WEb Development with Ruby on Rails/widgets/config/routes.rb
+++ Sustainable WEb Development with Ruby on Rails/widgets/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resurces :widgets
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'manufacturer/:id', to: 'manufacturers#show'
 end
