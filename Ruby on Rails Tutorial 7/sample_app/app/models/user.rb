d801704feb1e126a2ba89d9b6c5a10f8137f5aac
Patryk Rogala <rogala.patryk@pm.me>
2023-07-10 09:14:33 +0200

    Ruby on Rails Tutorial 7

--- Ruby on Rails Tutorial 7/sample_app/app/models/user.rb
+++ Ruby on Rails Tutorial 7/sample_app/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
 
@@ -43,5 +44,16 @@ class User < ApplicationRecord
 
   def forget
     update_attribute :remember_digest, nil
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- Ruby on Rails Tutorial 7/sample_app/test/integration/users_index_test.rb
+++ Ruby on Rails Tutorial 7/sample_app/test/integration/users_index_test.rb
@@ -15,16 +15,16 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
-       unless user == @admin
-         assert_select 'a[href=?]', user_path(user), text: 'delete'
-       end
-     end
-     assert_difference 'User.count', -1 do
-       delete user_path(@non_admin)
-       assert_response :see_other
-       assert_redirected_to users_url
-     end
-   end
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+      assert_response :see_other
+      assert_redirected_to users_url
+    end
+  end
 
   test "index as non-admin" do
     log_in_as(@non_admin)
