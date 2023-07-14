76c8bd921a3d0d8d3ad30dd9ea10bba90628b48c
Patryk Rogala <rogala.patryk@pm.me>
2023-07-14 19:01:32 +0200

    Restart repo

--- Sustainable WEb Development with Ruby on Rails/widgets/config/initializers/lograge.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.configure do
-  config.lograge.enabled = if !Rails.env.development? || ENV['LOGRAGE_IN_DEVELOPMENT'] == 'true'
-                             true
-                           else
-                             false
-                           end
-end
