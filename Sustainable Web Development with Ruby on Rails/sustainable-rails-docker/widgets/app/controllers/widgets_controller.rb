72b8351e06a149fcd08c50ef2076478ce7daec59
Patryk Rogala <rogala.patryk@pm.me>
2023-07-15 08:46:25 +0200

    Vanity URLs Should Redirect to a Canonical Route

--- Sustainable Web Development with Ruby on Rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
+++ Sustainable Web Development with Ruby on Rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
@@ -4,4 +4,11 @@ class WidgetsController < ApplicationController
                              menufacturer_id: rand(100),
                              name: "Widget #{params[:id]}")
   end
+
+  def index
+    @widgets = [
+      OpenStruct.new(id: 1, name: "Stembolt"),
+      OpenStruct.new(id: 2, name: "Flux Capacitor")
+    ]
+  end
 end
--- Sustainable Web Development with Ruby on Rails/sustainable-rails-docker/widgets/config/routes.rb
+++ Sustainable Web Development with Ruby on Rails/sustainable-rails-docker/widgets/config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-	resources :widgets, only: [ :show ]
+  resources :widgets, only: [ :show, :index ]
 
-  get "manufacturer/:id", to: "manufacturers#show"
+  ####
+  # Custom routes start here
+  #
+  # For each new custom route:
+  #
+  # * Be sure you have the canonical route declared above
+  # * Add the new custom route below the existing ones
+  # * Document why it's needed
+  # * Explain anything else non-standard
+
+  # Used in podcast ads for the 'amazing' campaign
+  get "/amazing", to: "widgets#index"
 end
