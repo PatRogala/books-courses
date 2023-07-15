6627d23d9d555ede5fb28fa37428813328af0176
Patryk Rogala <rogala.patryk@pm.me>
2023-07-15 09:12:03 +0200

    Don't Conflate Helpers with Your Domain

--- Sustainable Web Development with Ruby on Rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
+++ Sustainable Web Development with Ruby on Rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
@@ -13,6 +13,13 @@ class WidgetsController < ApplicationController
                              menufacturer_id: manufacturer.id,
                              manufacturer: manufacturer,
                              name: "Widget #{params[:id]}")
+    def @widget.widget_id
+      if self.id.to_s.length < 3
+        self.id.to_s
+      else
+        self.id.to_s[0..-3] + "." + self.id.to_s[-2..-1]
+      end
+    end
   end
 
   def index
