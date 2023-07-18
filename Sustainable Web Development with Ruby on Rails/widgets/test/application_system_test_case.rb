3fc6ba7785e893c697507af5b37d95394c59b6c9
Patryk Rogala <rogala.patryk@pm.me>
2023-07-18 17:08:20 +0200

    Setting Up Headless Chrome

--- Sustainable Web Development with Ruby on Rails/widgets/test/application_system_test_case.rb
+++ Sustainable Web Development with Ruby on Rails/widgets/test/application_system_test_case.rb
@@ -1,7 +1,34 @@
 require "test_helper"
+
+Capybara.register_driver :root_headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions": {
+      args: [
+        "headless",
+        "disable-gpu",
+        "no-sandbox",
+        "disable-dev-shm-usage",
+        "whitelisted-ips"
+      ]
+    },
+    "goog:loogingPrefs": { browser: "ALL" },
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities)
+end
+
 require "support/with_clues"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include TestSupport::WithClues
   driven_by :rack_test
+end
+
+# Use this as the base class for system tests that require
+# JavaScript or that otherwise need a real browser
+class BrowserSystemTestCase < ApplicationSystemTestCase
+  driven_by :root_headless_chrome, screen_size: [ 1400, 1400 ]
 end
