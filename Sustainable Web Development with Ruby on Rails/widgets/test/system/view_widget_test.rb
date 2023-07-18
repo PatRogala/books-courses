2c4954d1daa90f3552d81a84158f03defc15f5b9
Patryk Rogala <rogala.patryk@pm.me>
2023-07-18 16:26:31 +0200

    Use :rack_test for non-JavaScript User Flows

---
+++ Sustainable Web Development with Ruby on Rails/widgets/test/system/view_widget_test.rb
@@ -0,0 +1,7 @@
+require "application_system_test_case"
+
+class ViewWidgetTest < ApplicationSystemTestCase
+  test "we can see a list of widgets and choose one to view" do
+    # test goes here
+  end
+end
