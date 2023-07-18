7d92f9d584238f114005edcc764511d020dafdd2
Patryk Rogala <rogala.patryk@pm.me>
2023-07-18 20:54:00 +0200

    Instance Methods Should Implement Domain Concepts Derivable Directly from the Database

--- Sustainable Web Development with Ruby on Rails/widgets/app/models/widget.rb
+++ Sustainable Web Development with Ruby on Rails/widgets/app/models/widget.rb
@@ -1,3 +1,15 @@
 class Widget < ApplicationRecord
   belongs_to :manufacturer
+
+  def user_facing_identifier
+    id_as_string = self.id.to_s
+    if id_as_string.length < 3
+      return id_as_string
+    end
+
+    "%{first}.%{last_two}" % {
+      first: id_as_string[0..-3],
+      last_two: id_as_string[-2..-1]
+    }
+  end
 end
