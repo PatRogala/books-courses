a34cb4ef0484ad01bc23af9b65db319b24ce8a64
Patryk Rogala <rogala.patryk@pm.me>
2023-07-19 07:42:08 +0200

    Creating Correct Migrations

--- Sustainable Web Development with Ruby on Rails/widgets/lib/rails_ext/active_record_datetime_uses_timestamptz.rb
+++ Sustainable Web Development with Ruby on Rails/widgets/lib/rails_ext/active_record_datetime_uses_timestamptz.rb
@@ -1,6 +1,6 @@
 require "active_record/connection_adapters/postgresql_adapter.rb"
 
-ActiveRecord::ConnectionAdeptars::PostgreSQLAdapter::
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::
   NATIVE_DATABASE_TYPES[:datetime] = {
     name: "timestamptz"
   }
