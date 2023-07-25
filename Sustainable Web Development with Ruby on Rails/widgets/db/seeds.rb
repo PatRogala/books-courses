9724e6e33a8dd9f5f4afa0deaad2f275e32637ab
Patryk Rogala <rogala.patryk@pm.me>
2023-07-25 22:47:34 +0200

    Create Useful Seed Data for Development

--- Sustainable Web Development with Ruby on Rails/widgets/db/seeds.rb
+++ Sustainable Web Development with Ruby on Rails/widgets/db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if !Rails.env.development?
+  puts "[ db/seeds.rb ] Seed data is for development only. " +
+    "not #{Rails.env}"
+  exit 0
+end
+
+require "factory_bot"
+
+Widget.destroy_all
+Manufacturer.destroy_all
+Address.destroy_all
+WidgetStatus.destroy_all
+
+puts "[ db/seeds.rb ] Creating development data..."
+FactoryBot.create(:widget_status, name: "Fresh")
+10.times do
+  FactoryBot.create(:manufacturer)
+end
+cyberdyne = FactoryBot.create(:manufacturer, name: "Cyberdyne Systems")
+FactoryBot.create(:widget, name: "Stembolt", manufacturer: cyberdyne)
+puts "[ db/seeds.rb ] Done"
